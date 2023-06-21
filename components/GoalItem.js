3010801525f21cc9875ce04192c019488ed0bdef
Farah <mahboob.farah@gmail.com>
2023-06-21 11:04:51 +0200

    Fixed styling and readme.

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
     borderRadius: 6,
-    backgroundColor: '#5e0acc',
+    backgroundColor: '#54369a',
   },
   pressedItem: {
     opacity: 0.5,
